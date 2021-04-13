a60197ab96646a4576d7a982eb07e61882eda6b3
Subramanian Narayanan <nagusubra@gmail.com>
2021-04-13 03:42:00 -0600

    Fourth version commit: Edited the code

--- TFLite-PiCamera-od.py
+++ TFLite-PiCamera-od.py
@@ -19,7 +19,7 @@ class VideoStream:
     """
     def __init__(self, resolution=(640,480),framerate=60):
         # Initialize the PiCamera and the camera image stream
-        self.stream = cv2.VideoCapture('videos/test-video.h264')
+        self.stream = cv2.VideoCapture(0)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
@@ -117,14 +117,25 @@ time.sleep(1)
 
 cnt_enter = 0
 cnt_exit = 0
+total_persons = 0
+total_persons_previous = 0
+total_persons1 = 0
+total_persons_previous1 = 0
+brkcnt = 0
+
+
+text="WELCOME TO AI CUSTOMER COUNTER, please enter the store"
+os.system('espeak "'+text+'"')
+
+
 
 #for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 while True:
 
     current_count=0                                                 #"""Start timer (for calculating frame rate)"""
     t1 = cv2.getTickCount()
-    frame1 = videostream.read()                                     #"""Grab frame from video stream """
-    frame = frame1.copy()                                           #"""Acquire frame and resize to expected shape [1xHxWx3]"""
+    frame = videostream.read()                                     #"""Grab frame from video stream """
+    #frame = frame1.copy()                                           #"""Acquire frame and resize to expected shape [1xHxWx3]"""
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 
@@ -144,7 +155,12 @@ while True:
     scores1 = interpreter.get_tensor(output_details1[2]['index'])[0] # Confidence of detected objects
 
     # Loop over all detections and draw detection box if confidence is above minimum threshold
+    
+    total_persons_previous = total_persons
+    total_persons = 0
+    
     for i in range(len(scores1)):
+       
         if classes1[i].all() == 0.0:
 
             if ((scores1[i] > MIN_CONF_THRESH) and (scores1[i] <= 1.0)):
@@ -162,14 +178,16 @@ while True:
                 object_name = labels[int(classes1[i])] # Look up object name from "labels" array using class index
                 
                 if object_name == 'person':
-                    label = '%s: %d%%' % ('Customer', int(scores1[i]*100)) # Example: 'person: 72%'
+                    label = '%s: %d%%' % ('Person', int(scores1[i]*100)) # Example: 'person: 72%'
                     labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
                     label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
                     cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
                     cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-                    cnt_exit+=1
-
-    
+                    total_persons+=1   
+                    
+    if total_persons > total_persons_previous:
+        cnt_exit+= (total_persons - total_persons_previous)
+        
     frame_rgb2 = frame_rgb[0:720, 680:1280]
     frame_resized2 = cv2.resize(frame_rgb2, (width, height))
     input_data2 = np.expand_dims(frame_resized2, axis=0)
@@ -184,6 +202,9 @@ while True:
     scores = interpreter.get_tensor(output_details2[2]['index'])[0] # Confidence of detected objects
 
     # Loop over all detections and draw detection box if confidence is above minimum threshold
+    total_persons_previous1 = total_persons1
+    total_persons1 = 0
+    
     for i in range(len(scores)):
         if classes[i].all() == 0.0:
 
@@ -202,14 +223,25 @@ while True:
                 object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
                 
                 if object_name == 'person':
-                    label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                    label = '%s: %d%%' % ('Customer', int(scores[i]*100)) # Example: 'person: 72%'
                     labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
                     label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
                     cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
                     cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-                    cnt_enter+=1
+                    total_persons1+=1   
+                    
+    if total_persons1 > total_persons_previous1:
+        cnt_enter+= (total_persons1 - total_persons_previous1)
+
+    if cnt_enter >=3 and brkcnt <2:
+        brkcnt+=1
+        cv2.rectangle(frame, (650,35), (1270,710), (0, 0, 255), 6)
+        text="The store has reahced macimum capacity of 3 customers. Please wait in a line"
+        os.system('espeak "'+text+'"')
 
 
+    if cnt_enter >=3:
+        cv2.rectangle(frame, (650,10), (1270,710), (0, 0, 255), 6)
 
     cv2.putText(frame,'Frame rate per sec: {0:.2f}'.format(frame_rate_calc),(15,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,55),2,cv2.LINE_AA)
     cv2.line(frame, (640,0), (640,720), (0,250,250), 5)
@@ -217,9 +249,11 @@ while True:
     cv2.putText (frame,'Entered : '+ str(cnt_enter),(10,25),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3,cv2.LINE_AA)
     cv2.putText (frame,'Exit : '+ str(cnt_exit),(10,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3,cv2.LINE_AA)
 
+    cv2.putText (frame,'Enterance',(780,680),cv2.FONT_HERSHEY_DUPLEX,3,(255,0,0),3,cv2.LINE_AA)
+    cv2.putText (frame,'Exit',(10,680),cv2.FONT_HERSHEY_DUPLEX,3,(255,0,0),3,cv2.LINE_AA)
+
     cv2.putText (frame,'Customer Counter, Welcome to UofC!',(350,25),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3,cv2.LINE_AA)
     #cv2.putText (frame,'Total Customer Count : ' + str(current_count),(450,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3,cv2.LINE_AA)
-    # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object Detector', frame)
 
     # Calculate framerate
